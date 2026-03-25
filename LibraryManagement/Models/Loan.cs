ca82c136d33e42dafe2f1db819ff1a2ca7f35bd7
Salidzhan <salicanosman2000@gmail.com>
2026-03-25 23:11:11 +0200

    Add Loan model with dates and relations

--- LibraryManagement/Models/Loan.cs
+++ LibraryManagement/Models/Loan.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace LibraryManagement.Models
 {
-    internal class Loan
+    public class Loan
     {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int MemberId { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
     }
 }
