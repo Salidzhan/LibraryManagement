93f5bffedc162d1416c466e7432ffd07f7d60980
Salidzhan <salicanosman2000@gmail.com>
2026-03-25 23:12:04 +0200

    Create Member model

--- LibraryManagement/Models/Member.cs
+++ LibraryManagement/Models/Member.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace LibraryManagement.Models
 {
-    internal class Member
+    public class Member
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int ActiveLoansCount { get; set; }
     }
 }
