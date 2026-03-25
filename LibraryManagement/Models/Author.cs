6baab592bfe45e5a1c70a9ca63585689411b7d30
Salidzhan <salicanosman2000@gmail.com>
2026-03-25 23:09:08 +0200

    Add Author model

--- LibraryManagement/Models/Author.cs
+++ LibraryManagement/Models/Author.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace LibraryManagement.Models
 {
-    internal class Author
+    public class Author
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
