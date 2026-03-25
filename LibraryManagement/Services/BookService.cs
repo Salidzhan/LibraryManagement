97ea628868c0618187212dc57d82148a9130314b
Salidzhan <salicanosman2000@gmail.com>
2026-03-25 23:12:24 +0200

    Implement BookService with CRUD operations

--- LibraryManagement/Services/BookService.cs
+++ LibraryManagement/Services/BookService.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibraryManagement.Models;
 
 namespace LibraryManagement.Services
 {
-    internal class BookService
+    public class BookService
     {
+        private List<Book> books = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void UpdateBook(Book book)
+        {
+            var existing = books.FirstOrDefault(b => b.Id == book.Id);
+            if (existing != null)
+            {
+                existing.Title = book.Title;
+                existing.Genre = book.Genre;
+                existing.AuthorId = book.AuthorId;
+                existing.AvailableCopies = book.AvailableCopies;
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book != null)
+            {
+                books.Remove(book);
+            }
+        }
+
+        public List<Book> GetAll()
+        {
+            return books;
+        }
     }
 }
