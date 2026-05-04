b6ecef02cbd7fc67627c3c4d1d139bb8498d9174
Salidzhan <salicanosman2000@gmail.com>
2026-05-04 15:11:20 +0300

    add interface author repo

---
+++ LibraryManagement/Services/Interfaces1/IAuthorRepository.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Services.Interfaces1
+{
+    public interface IAuthorRepository
+    {
+        List<Author> GetAll();
+        void Save(Author author);
+    }
+}
