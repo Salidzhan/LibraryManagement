4c61036267d9b351f7b011baee94a29cebce7bb4
desko-petkova <desko.petkova@gmail.com>
2026-05-04 14:05:49 +0300

    LibraryStorage and FileStorage classes added

---
+++ LibraryManagement/Data/FileStorage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace LibraryManagement.Data
+{
+    public class FileStorage
+    {
+        private readonly string path;
+
+        public FileStorage(string path)
+        {
+            this.path = path;
+        }
+
+        public LibraryStorage Load()
+        {
+            if (!File.Exists(path))
+            {
+                return new LibraryStorage();
+            }
+
+            var json = File.ReadAllText(path);
+
+            var storage = JsonSerializer.Deserialize<LibraryStorage>(json);
+            if (storage == null)
+                throw new Exception("Deserialization returned null.");
+
+
+            return storage;
+
+        }
+
+        public void Save(LibraryStorage storage)
+        {
+
+            var json = JsonSerializer.Serialize(storage);
+
+            File.WriteAllText(path, json);
+        }
+    }
+}
---
+++ LibraryManagement/Data/LibraryStorage.cs
@@ -0,0 +1,21 @@
+﻿using LibraryManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Data
+{
+    public class LibraryStorage
+    {
+        public int NextId { get; set; } = 1;
+
+        public List<Book> Books { get; set; } = new List<Book>();
+
+        public List<Author> Authors { get; set; } = new List<Author>();
+
+        public List<Member> Members { get; set; } = new List<Member>();
+    }
+}
