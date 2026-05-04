71dff5abe0344aef7005ffd3c5dbf0c06c04f406
Salidzhan <salicanosman2000@gmail.com>
2026-05-04 15:00:13 +0300

    added book interface

---
+++ LibraryManagement/Services/Interfaces1/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using LibraryManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Services.Interfaces1
+{ 
+        public interface IBookRepository
+        {
+            List<Book> GetAll();
+            void Save(Book book);
+        }
+    
+}
