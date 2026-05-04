9e58de6e75cd696e2e9dcc2c7eaa8607d13570ce
Salidzhan <salicanosman2000@gmail.com>
2026-05-04 15:28:12 +0300

    Add console ui add book register member borrow book search book reports add connection between file storage and library service

--- LibraryManagement/ConsoleUi/Program.cs
+++ LibraryManagement/ConsoleUi/Program.cs
@@ -12,7 +12,25 @@ namespace LibraryManagement
     internal class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            var storage = new FileStorage("library.json");
+
+            var bookRepo = new FileStorage<Book>(storage);
+            var memberRepo = new FileStorage<Member>(storage);
+            var loanRepo = new FileStorage<Loan>(storage);
+            var reservationRepo = new FileStorage<Reservation>(storage);
+
+            var service = new LibraryService(bookRepo, memberRepo, loanRepo, reservationRepo);
+
+            var ui = new LibraryUi(service);
+            ui.Run();
+
+          Console.WriteLine("1 Add Book");
+            Console.WriteLine("2 Register Member");
+            Console.WriteLine("3 Borrow Book");
+            Console.WriteLine("4 Return Book");
+            Console.WriteLine("5 Search Book");
+            Console.WriteLine("6 Reports");
 
         }
     }
